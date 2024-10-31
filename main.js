6716af58bb36427453568aa50a5d168a63c6f429
kreit <reitenbaughkm@gmail.com>
2024-10-31 10:29:17 -0500

    Initial commit  Changes to be committed: 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   index.html 	new file:   main.js 	new file:   meme-duck-i-cant-believe-you-6ot-plastic-surgery.jpeg

---
+++ main.js
@@ -0,0 +1,33 @@
+// 1. Alter the text color of the paragraph to a shade of aqua.
+const paragraph = document.querySelector('p')
+paragraph.style.color = "aquamarine"
+
+
+// 2. Adjust the font size of the heading to 42 pixels.
+const heading = document.querySelector('h1')
+heading.style.fontSize = '42px'
+
+// 3. Replace the text in the paragraph with the new Lorem Ipsum content.
+paragraph.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
+
+// 4. Reduce the opacity of the 13th item to 30% transparency.
+const list = document.querySelector('#item-13')
+list.style.opacity = "30%"
+
+// 5. Modify the text of the 3rd item to read "Greetings!"
+const lists = document.querySelector('item-3')
+lists.innerText = "Greetings!"
+
+// 6. Update the image below the list to display the new image.
+
+// 7. Resize the new image to a height of 250 pixels.
+
+// 8. Integrate an additional image using innerHTML below the existing image.
+
+// 9. Adjust the height of the new image to 280 pixels programmatically.
+
+// 10. Append a 16th <li> element to the list using innerHTML.
+
+// 11. Assign the same class as the other list items to the new <li> element using JavaScript.
+
+// 12. Populate the new <li> with the text "Never gonna give you up."
